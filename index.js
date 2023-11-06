b636bcfc0d4048dd87b9375969887365d859a6cc
Devin Elliot Gomez <devinelliotgomez@gmail.com>
2023-11-06 13:12:12 -0600

    finished lab

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  let titleCasedNames = [];
+
+  tutorials.forEach((tutorialName) => {
+    let words = tutorialName.split(" ");
+    let capitalizedWords = words.map(
+      (word) => word.charAt(0).toUpperCase() + word.slice(1)
+    );
+    let titleCasedName = capitalizedWords.join(" ");
+    titleCasedNames.push(titleCasedName);
+  });
+
+  return titleCasedNames;
 }
